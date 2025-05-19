d519fcd5d266007101dd4051599deb415084cb54
Chlukas <lukasnackmayr@proton.me>
2025-05-19 16:17:07 +0200

    feat: added basic loading of config file

---
+++ internal/config/config.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type ScrapeTarget struct {
+	Name string `yaml:"name"`
+	StartUrl string `yaml:"start_url"`
+	AllowedDomains []string `yaml:"allowed_domains"`
+	ItemSelector string `yaml:"item_selector"`
+}
+
+type Config struct {
+	ScrapeTarget []ScrapeTarget `yaml:"scrape_targets"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	cfgf, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var config Config
+	err = yaml.Unmarshal(cfgf, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
